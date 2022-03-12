329c09b079700c8d97fdb465a06efb1eddeda2eb
Jacob Christ <jacob@pontech.com>
2022-03-12 15:14:40 -0800

    Added SpiralWait state to Enemy.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,17 +7,23 @@ public class Enemy : MonoBehaviour
     public enum EnemyStyle {
         FollowPlayer,
         RunFromPlayer,
-        Spiral
+        Spiral,
+        SpiralWait
     }
 
     public EnemyStyle Style;
     public Transform Player;
-    public float speed;
-    public float SpiralSpeed;
-    public float SpiralAmplitutde;
+    public float Speed = 1;
+    public float SpiralSpeedX = 1;
+    public float SpiralSpeedY = 1;
+    public float SpiralAmplitutdeX;
+    public float SpiralAmplitutdeY;
 
     public int LightAccumulation = 0;
     public int LightThreshold = 100;
+    public int LightClamp = 200;
+
+    private float theta = 0;
 
     private Vector3 inital_position;
 
@@ -35,24 +41,31 @@ public class Enemy : MonoBehaviour
         Vector3 spiral = Vector3.zero;
         Vector3 direction = Vector3.Normalize(transform.position - Player.position);
 
+        if (LightAccumulation > LightClamp) LightAccumulation = LightClamp;
+        if (LightAccumulation > 0)
+            LightAccumulation--;
 
         switch (Style)
         {
             case EnemyStyle.FollowPlayer:
-                move = direction * -speed * Time.deltaTime;
+                move = direction * -Speed * Time.deltaTime;
                 transform.position += move;
                 if (LightAccumulation > LightThreshold) Style = EnemyStyle.RunFromPlayer;
                 break;
             case EnemyStyle.RunFromPlayer:
-                move = direction * speed * Time.deltaTime;
+                move = direction * Speed * Time.deltaTime;
                 transform.position += move;
-                LightAccumulation--;
-                if (LightAccumulation < 0) Style = EnemyStyle.FollowPlayer;
+                if (LightAccumulation <= 0) Style = EnemyStyle.FollowPlayer;
                 break;
+
             case EnemyStyle.Spiral:
-                float amp = SpiralAmplitutde; // * direction.magnitude;
-                spiral = new Vector2(amp * Mathf.Sin(Time.realtimeSinceStartup * SpiralSpeed), amp * Mathf.Cos(Time.realtimeSinceStartup * SpiralSpeed));
+                theta += Time.deltaTime * 0.1f;
+                spiral = new Vector2(SpiralAmplitutdeX * Mathf.Sin(theta * SpiralSpeedX), SpiralAmplitutdeY * Mathf.Cos(theta * SpiralSpeedY));
                 transform.position = inital_position + spiral;
+                if (LightAccumulation > LightThreshold) Style = EnemyStyle.SpiralWait;
+                break;
+            case EnemyStyle.SpiralWait:
+                if (LightAccumulation <= 0) Style = EnemyStyle.Spiral;
                 break;
         }
         Debug.DrawLine(transform.position, Player.position, Color.red);

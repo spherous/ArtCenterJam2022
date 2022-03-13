1e0ef8c62d39d76f6e73119634a41effb0ef5711
Vanna <vannaerr404@gmail.com>
2022-03-13 11:07:33 -0500

    b

--- Assets/Scripts/MenuEnemies.cs
+++ Assets/Scripts/MenuEnemies.cs
@@ -9,31 +9,41 @@ public class MenuEnemies : MonoBehaviour
     public GroupFader[] enemies;
     bool goingDown;
 
+    private void Start()
+    {
+        goingDown = true;
+    }
     private void Update()
     {
         //Debug.Log(globalLight.intensity);
         int i = Mathf.CeilToInt(globalLight.intensity * 100);
         Debug.Log(i);
-        /*if(i == 25)
+        /*if(i == 25 && goingDown)
         {
             enemies[6].FadeIn();
             enemies[7].FadeIn();
         }
-        else if (i == 15)
+        else if (i == 15 && goingDown)
         {
             enemies[4].FadeIn();
             enemies[5].FadeIn();
 
         }
-        else if (i == 10)
+        else if (i == 10 && goingDown)
         {
             enemies[2].FadeIn();
             enemies[3].FadeIn();
         }
-        else if (i == 5)
+        else if (i == 5 && goingDown)
         {
             enemies[0].FadeIn();
             enemies[1].FadeIn();
+            goingDown = false;
+        }
+        if (i == 6 && !goingDown)
+        {
+            enemies[7].FadeOut;
+            enemies[6].FadeOut;
         }*/
     }
 }

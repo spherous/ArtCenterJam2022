8c94d5a970858380435a2582b4234d60a1d6dff2
Vanna <vannaerr404@gmail.com>
2022-03-13 11:00:41 -0500

    a

--- Assets/Scripts/MenuEnemies.cs
+++ Assets/Scripts/MenuEnemies.cs
@@ -7,49 +7,33 @@ public class MenuEnemies : MonoBehaviour
 {
     [SerializeField] private Light2D globalLight;
     public GroupFader[] enemies;
+    bool goingDown;
 
     private void Update()
     {
         //Debug.Log(globalLight.intensity);
         int i = Mathf.CeilToInt(globalLight.intensity * 100);
         Debug.Log(i);
-        /*if(i = 25)
+        /*if(i == 25)
         {
             enemies[6].FadeIn();
             enemies[7].FadeIn();
         }
-        else if (i < 15 && i > 10)
+        else if (i == 15)
         {
-            enemies[0].FadeOut();
-            enemies[1].FadeOut();
-            enemies[2].FadeOut();
-            enemies[3].FadeOut();
             enemies[4].FadeIn();
             enemies[5].FadeIn();
 
         }
-        else if (i < 10 && i > 5)
+        else if (i == 10)
         {
-            enemies[0].FadeOut();
-            enemies[1].FadeOut();
             enemies[2].FadeIn();
             enemies[3].FadeIn();
         }
-        else if (i < 5)
+        else if (i == 5)
         {
             enemies[0].FadeIn();
             enemies[1].FadeIn();
-        }
-        else if (i > 25)
-        {
-            enemies[0].FadeOut();
-            enemies[1].FadeOut();
-            enemies[2].FadeOut();
-            enemies[3].FadeOut();
-            enemies[4].FadeOut();
-            enemies[5].FadeOut();
-            enemies[6].FadeOut();
-            enemies[7].FadeOut();
         }*/
     }
 }
